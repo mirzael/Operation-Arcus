885aa5f537b31e6e629680b7f0b0ff0e60714883
mirzael <michael.y.irizarry@gmail.com>
2014-10-08 11:04:08 -0400

    Added collisions to enemy ships

---
+++ Assets/Scripts/Enemy Scripts/EnemyDeath.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		Destroy (col.gameObject);
+		Destroy (gameObject);
+	}
+}
