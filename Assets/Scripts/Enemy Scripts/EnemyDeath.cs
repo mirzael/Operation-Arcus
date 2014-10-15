2971642ce91eb9f2b2cb88750a81552219676c3d
mirzael <michael.y.irizarry@gmail.com>
2014-10-14 23:23:12 -0400

    Added Gravitational field for orange weapon

--- Assets/Scripts/Enemy Scripts/EnemyDeath.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDeath.cs
@@ -16,21 +16,34 @@ public class EnemyDeath : MonoBehaviour {
 	void OnCollisionEnter(Collision col){
 		//AOE, BABY
 		if (col.gameObject.tag == "Red") {
-			Vector3 center = col.contacts[0].point;
-			var collisions = Physics.OverlapSphere(center, 2.5f);
-			for(int i = 0; i < collisions.Length; i++){
-				if(collisions[i].gameObject.layer == LayerMask.NameToLayer("Enemy")){
-					var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					sphere.renderer.material = (Material) Resources.Load("Materials/AoeBlasts/RedBlast", typeof(Material));
-					sphere.layer = LayerMask.NameToLayer("Character Bullet");
-					sphere.transform.position = collisions[i].transform.position;
-					sphere.transform.localScale = new Vector3(2.5f,2.5f,2.5f);
-					Destroy (sphere, 0.5f);
-					Destroy(collisions[i].gameObject);
-				}
+			CreateAoe (col.contacts[0].point, (Material) Resources.Load("Materials/AoeBlasts/RedBlast", typeof(Material)), 2.5f, 0.5f);
+		}else if(col.gameObject.tag == "Orange"){
+			if(gameObject.tag == "Blue"){
+				CreateAoe (col.contacts[0].point, (Material) Resources.Load("Materials/AoeBlasts/OrangeBlast", typeof(Material)), 10f, 0.25f, true);
+			}else{
+				CreateAoe (col.contacts[0].point, (Material) Resources.Load("Materials/AoeBlasts/OrangeBlast", typeof(Material)), 4f, 0.5f);
 			}
 		}
 		Destroy (col.gameObject);
 		Destroy (gameObject);
+	}
+
+	void CreateAoe(Vector3 center, Material mat, float radius, float duration, bool gravity = false){
+		var collisions = Physics.OverlapSphere(center, radius);
+		for(int i = 0; i < collisions.Length; i++){
+			if(collisions[i].gameObject.layer == LayerMask.NameToLayer("Enemy")){
+				var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				sphere.renderer.material = mat;
+				sphere.transform.position = collisions[i].transform.position;
+				sphere.transform.localScale = new Vector3(radius, radius, radius);
+				if(gravity){ 
+					sphere.AddComponent<GravityField>();
+				}else{
+					sphere.layer = LayerMask.NameToLayer("Character Bullet");
+					Destroy(collisions[i].gameObject);
+				}
+				Destroy (sphere, duration);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Main Character Scripts/GravityField.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityField : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		foreach (Collider collider in Physics.OverlapSphere(transform.position, 100f)) {
+			if(collider.gameObject.layer == LayerMask.NameToLayer("Enemy")){
+				// calculate direction from target to me
+				Vector3 forceDirection = transform.position - collider.transform.position;
+				
+				// apply force on target towards me
+				collider.rigidbody.AddForce(forceDirection.normalized * 350 * Time.fixedDeltaTime);
+			}
+		}
+	}
+}
