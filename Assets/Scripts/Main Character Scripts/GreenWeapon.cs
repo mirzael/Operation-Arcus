1d6f5bcdb1f47fe141ad5c5c7a466500980bdd65
mirzael <michael.y.irizarry@gmail.com>
2014-10-17 17:23:57 -0400

    Green weapon implemented properly

---
+++ Assets/Scripts/Main Character Scripts/GreenWeapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreenWeapon : MonoBehaviour {
+
+	public float amplitude = 5f;
+	public float degreesPerSec;
+	public float degrees = 0;
+	public float ySpeed;
+	Vector3 centerPos;
+
+
+	// Use this for initialization
+	void Start () {
+		centerPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		centerPos += Vector3.up * ySpeed / 100;
+		degrees = Mathf.Repeat(degrees + (Time.deltaTime * degreesPerSec), 360.0f);
+		var radians = degrees * Mathf.Deg2Rad;
+
+		// Offset by sin wave
+		Vector3 offset = new Vector3(amplitude * Mathf.Sin(radians), 0.0f, 0.0f);
+		transform.position = centerPos + offset;
+	}
+}
--- Assets/Scripts/Main Character Scripts/MainCharacterDriver.cs
+++ Assets/Scripts/Main Character Scripts/MainCharacterDriver.cs
@@ -30,7 +30,7 @@ public class MainCharacterDriver : MonoBehaviour {
 	const float POWER_INC = 25.0f;
 	const float TRANSFORM_AMOUNT = 50f;
 	const int PROJECTILE_DISTANCE = 2;
-	const int GREEN_PROJECTILE_DISTANCE = 50;
+	const int GREEN_DEGREES_PER_SEC = 720;
 	public static float powerRed = 0.0f;
 	public static float powerBlue = 0.0f;
 	public static float powerYellow = 0.0f;
@@ -75,7 +75,7 @@ public class MainCharacterDriver : MonoBehaviour {
 		//Create the special forms
 		orangeForm = new Form (shipSpeed, 0.6f, projectiles [3], 100f, mats [3], ShipColor.ORANGE);
 		purpleForm = new Form (shipSpeed * 0.75f, 0.2f, projectiles [4], 75f, mats [4], ShipColor.PURPLE);
-		greenForm = new Form (shipSpeed * 1.5f, 0.1f, projectiles [5], 0f, mats [5], ShipColor.GREEN);
+		greenForm = new Form (shipSpeed * 1.5f, 0.1f, projectiles [5], 50f, mats [5], ShipColor.GREEN);
 
 		//Set the current form to the first form
 		currentForm = forms[0];
@@ -195,8 +195,7 @@ public class MainCharacterDriver : MonoBehaviour {
 			float radToDeg =  Mathf.PI / 180;
 			GameObject[] blast = new GameObject[numProjectiles];
 			Debug.Log (currentForm.projectile.transform.rotation.x);
-			for (int i = 0; i < numProjectiles; i++)
-			{
+			for(int i = 0; i < numProjectiles; i++){
 				float trajectoryDegree = 90 + (projectileSpreadAngle / 2 - angleBetweenProjectiles * i);
 				float currentAngularVelocity = Mathf.Cos(trajectoryDegree * radToDeg);
 				blast[i] = (GameObject)Instantiate(currentForm.projectile, transform.position + Vector3.up * PROJECTILE_DISTANCE, currentForm.projectile.transform.rotation);
@@ -216,7 +215,14 @@ public class MainCharacterDriver : MonoBehaviour {
 			moveScript.projectileSpeed = currentForm.projectileSpeed;
 			break;
 		case ShipColor.GREEN:
-			projectile = (GameObject)Instantiate(currentForm.projectile, transform.position + Vector3.up * PROJECTILE_DISTANCE, currentForm.projectile.transform.rotation);
+			var gProj = new GameObject[3];
+			gProj[0] = (GameObject)Instantiate(currentForm.projectile, transform.position + Vector3.up * PROJECTILE_DISTANCE, currentForm.projectile.transform.rotation);
+			gProj[1] = (GameObject)Instantiate(currentForm.projectile, transform.position + Vector3.up * PROJECTILE_DISTANCE, currentForm.projectile.transform.rotation);
+			gProj[2] = (GameObject)Instantiate(currentForm.projectile, transform.position + Vector3.up * PROJECTILE_DISTANCE, currentForm.projectile.transform.rotation);
+			sinBullet(gProj[0].AddComponent<GreenWeapon>(), false);
+			sinBullet(gProj[1].AddComponent<GreenWeapon>(), true);
+			var gmoveScript = gProj[2].AddComponent<MoveProjectile>();
+			gmoveScript.projectileSpeed = currentForm.projectileSpeed;
 			break;
 		}
 	}
@@ -225,6 +231,12 @@ public class MainCharacterDriver : MonoBehaviour {
 		currentForm = form;
 		renderer.material = currentForm.material;
 		currentCooldown = currentForm.getCooldown();
+	}
+
+	void sinBullet(GreenWeapon weapon, bool isNegative){
+		weapon.amplitude = isNegative ? -weapon.amplitude : weapon.amplitude;
+		weapon.ySpeed = currentForm.getSpeed();
+		weapon.degreesPerSec = GREEN_DEGREES_PER_SEC;
 	}
 
 }
