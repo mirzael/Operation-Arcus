9ecb92e9bde7376092d9cbd317b7d447a509fa7f
adkscherch <adkscherch@gmail.com>
2014-10-19 00:30:29 -0400

    Enemy bullet patterns now fire at a reasonable pace and amount of bullets.

---
+++ Assets/Scripts/Enemy Scripts/Wave.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+	float cooldown;
+	public GameObject projectile;
+	float currentCooldown;
+
+
+	// Use this for initialization
+	void Start () {
+		projectile = gameObject.GetComponent<Shooter> ().projectile;
+		currentCooldown = 20;
+		cooldown = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentCooldown < cooldown) 
+			{
+			var proj = new GameObject();
+			currentCooldown = currentCooldown + 1;
+			int aVal = (int) currentCooldown % 10;
+			switch (aVal)
+				{
+				case 0:
+					proj = (GameObject)Instantiate (projectile, gameObject.transform.position + Vector3.left * 2, transform.rotation);
+					proj.rigidbody.velocity = Vector3.left * 25 + Vector3.up * 5;
+					break;
+				case 1:
+					proj = (GameObject)Instantiate (projectile, gameObject.transform.position + Vector3.left * 2, transform.rotation);
+					proj.rigidbody.velocity = Vector3.left * 25 + Vector3.up;
+					break;
+				case 2:
+					proj = (GameObject)Instantiate (projectile, gameObject.transform.position + Vector3.left * 2, transform.rotation);
+					proj.rigidbody.velocity = Vector3.left * 25 + Vector3.up * -5;
+					break;
+				}
+			}
+	}
+
+	public virtual void resetCooldown()
+		{
+		currentCooldown = 0;
+		}
+}
