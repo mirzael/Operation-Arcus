3e5584ef0f76b1c0f5e63f7dace5e1bada291650
adkscherch <adkscherch@gmail.com>
2014-11-02 13:37:10 -0500

    Added some bullet patterns, enemy wave patterns were changed as part of this.

---
+++ Assets/Scripts/Enemy Scripts/DualPulseWave.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualPulseWave : Wave {
+	
+	float cooldown;
+	float currentCooldown;
+	
+	// Use this for initialization
+	void Start () {
+		projectile = gameObject.GetComponent<Shooter> ().projectile;
+		currentCooldown = 0;
+		cooldown = 120;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentCooldown % 20 == 0 && currentCooldown < cooldown) {
+			GameObject[] proj = new GameObject[3];
+			int aVal = (int)currentCooldown % 40;
+			Debug.Log (aVal.ToString() + "IVE GOT STUFF");
+			switch (aVal) {
+			case 0:
+				proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.left * .6f, transform.rotation);
+				proj [0].rigidbody.velocity = Vector3.down * 15;
+				proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.left * .6f, transform.rotation);
+				proj [1].rigidbody.velocity = Vector3.down * 15 + Vector3.left * 2f;
+				proj [2] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.left * .6f, transform.rotation);
+				proj [2].rigidbody.velocity = Vector3.down * 15 + Vector3.left * -2f;
+				break;
+			case 20:
+				proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.right * .6f, transform.rotation);
+				proj [0].rigidbody.velocity = Vector3.down * 15;
+				proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.right * .6f, transform.rotation);
+				proj [1].rigidbody.velocity = Vector3.down * 15 + Vector3.left * -2f;
+				proj [2] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2 + Vector3.right * .6f, transform.rotation);
+				proj [2].rigidbody.velocity = Vector3.down * 15 + Vector3.left * -2f;
+				break;
+			}
+		}
+		currentCooldown = currentCooldown + 1;
+	}
+	
+	public override void resetCooldown()
+	{
+		currentCooldown = 0;
+	}
+
+}
---
+++ Assets/Scripts/Enemy Scripts/TrackNetWave.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackNetWave : Wave {
+	
+	float cooldown;
+	float currentCooldown;
+	GameObject player;
+	Vector3 direction;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		projectile = gameObject.GetComponent<Shooter> ().projectile;
+		currentCooldown = 0;
+		cooldown = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null && currentCooldown % 20 == 0 && currentCooldown < cooldown) 
+		{
+			int aVal = (int)currentCooldown / 20;
+			//if (aVal == 0)
+			//{
+				var heading = player.transform.position - transform.position;
+				var distance = heading.magnitude;
+				direction = heading / distance;
+				Debug.Log ("DIRECTION: " + direction.ToString());
+			//}
+			GameObject[] proj = new GameObject[5];
+			switch (aVal) 
+				{
+				case 0:
+					proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [0].rigidbody.velocity = direction * 8;
+					break;
+				case 1:
+					proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [0].rigidbody.velocity = direction * 8 + Vector3.left * .8f;
+					proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [1].rigidbody.velocity = direction * 8 + Vector3.left * -.8f;
+					break;
+				case 2:
+					proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [0].rigidbody.velocity = direction * 8 + Vector3.left * 1.6f;
+					proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [1].rigidbody.velocity = direction * 8;
+					proj [2] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [2].rigidbody.velocity = direction * 8 + Vector3.left * -1.6f;
+					break;
+				case 3:
+					proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [0].rigidbody.velocity = direction * 8 + Vector3.left * 2.4f;
+					proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [1].rigidbody.velocity = direction * 8 + Vector3.left * .8f;
+					proj [2] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [2].rigidbody.velocity = direction * 8 + Vector3.left * -.8f;
+					proj [3] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [3].rigidbody.velocity = direction * 8 + Vector3.left * -2.4f;
+					break;
+				case 4:
+					proj [0] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [0].rigidbody.velocity = direction * 8 + Vector3.left * 3.2f;
+					proj [1] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [1].rigidbody.velocity = direction * 8 + Vector3.left * 1.6f;
+					proj [2] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [2].rigidbody.velocity = direction * 8;
+					proj [3] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [3].rigidbody.velocity = direction * 8 + Vector3.left * -1.6f;
+					proj [4] = (GameObject)Instantiate (projectile, transform.position + Vector3.down * 2, transform.rotation);
+					proj [4].rigidbody.velocity = direction * 8 + Vector3.left * -3.2f;
+					break;
+				}
+		}
+		currentCooldown = currentCooldown + 1;
+	}
+	
+	public override void resetCooldown()
+	{
+		currentCooldown = 0;
+	}
+	
+}
