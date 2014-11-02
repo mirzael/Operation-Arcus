3f02ddf08452ef20f17adc11e8bffdfe4bfdf8ac
Jon Hopkins <jhopkins720@gmail.com>
2014-11-02 14:50:09 -0500

    Adding main menu and credits scenes. Instructions and hiscores menus are just placeholders at the moment.

--- Assets/Scripts/Background Scripts/WinLoseGUI.cs
+++ Assets/Scripts/Background Scripts/WinLoseGUI.cs
@@ -15,7 +15,11 @@ public class WinLoseGUI : MonoBehaviour {
 		guiStyle.alignment = TextAnchor.UpperCenter;
 		Rect textArea = new Rect(0, 0, Screen.width, Screen.height);
 		if (win) {
-			GUI.Label(textArea, "You Won! Press R to play again", guiStyle);
+			if (Spawner.level < Spawner.MAX_LEVELS) {
+				GUI.Label(textArea, "You Won! Press R to play again", guiStyle);
+			} else {
+				GUI.Label(textArea, "You won the war! Press R to continue", guiStyle);
+			}
 		} else {
 			GUI.Label(textArea, "You Lost! Press R to play again", guiStyle);
 		}
@@ -31,6 +35,10 @@ public class WinLoseGUI : MonoBehaviour {
 			Debug.Log("Restarting game");
 			if (win) {
 				Spawner.level++;
+				if (Spawner.level > Spawner.MAX_LEVELS) {
+					Application.LoadLevel("Credits");
+					return;
+				}
 			} else {
 				Spawner.level = 1;
 			}
