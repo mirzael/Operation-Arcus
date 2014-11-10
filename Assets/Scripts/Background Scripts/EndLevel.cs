3f05db4a63db8e0cbeeb483781b95b7d64b778e7
Jon Hopkins <jhopkins720@gmail.com>
2014-11-10 18:43:41 -0500

    Checking for enemies by layer rather than by name

--- Assets/Scripts/Background Scripts/EndLevel.cs
+++ Assets/Scripts/Background Scripts/EndLevel.cs
@@ -4,9 +4,8 @@ public class EndLevel : MonoBehaviour {
 	public void Update() {
 		var enemies = GameObject.FindObjectsOfType(typeof(GameObject));
 		foreach (GameObject go in enemies) {
-			if (go.name.StartsWith("Enemy") || go.name.StartsWith("R") || go.name.StartsWith("Y") || go.name.StartsWith("B")) {
-				// only counts as enemy if it is the base object (UI has some parts with conflicting names)
-				if (go.transform.parent == null) return;
+			if (go.layer == 8) {
+				return;
 			}
 		}
 		WinLoseGUI gui = GameObject.Find("Main Camera").AddComponent<WinLoseGUI>();
