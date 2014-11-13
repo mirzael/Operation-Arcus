d52e7874185991681b2e7f7474adaac9bef9296f
adkscherch <adkscherch@gmail.com>
2014-11-13 11:28:55 -0500

    Altered the UI so the secondary colors have materials set for lighting up upon being able to activate the relevant secondary.

---
+++ Assets/Arcus_Updated/Animation Variables.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+
+// Transform Var Values
+// 1 = Red
+// 2 = Blue
+// 3 = Yellow
+// 4 = Purple
+// 5 = Orange
+// Just use 0 for Green and Rainbow right now. We're having technical difficulties with Green.
+function Start () {
+
+}
+
+function Update () {
+
+}
