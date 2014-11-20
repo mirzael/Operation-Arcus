1315ec22a62bde027f4330ce397dbb51ad71d98e
Jon Hopkins <jhopkins720@gmail.com>
2014-11-19 21:51:58 -0500

    More rainbowy rainbow mode

--- Assets/Scripts/Main Character Scripts/WeaponScripts/RainbowWeapon.cs
+++ Assets/Scripts/Main Character Scripts/WeaponScripts/RainbowWeapon.cs
@@ -4,7 +4,12 @@ using System.Collections;
 
 public class RainbowWeapon : MonoBehaviour {
 	public float damage;
-
+	
+	public void Start() {
+		Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+		renderer.material.color = newColor;
+	}
+	
 	void OnCollisionEnter(Collision col){
 		col.gameObject.BroadcastMessage ("OnHit", new WeaponDamage{tag=tag, damage=damage});
 		Destroy (gameObject);
