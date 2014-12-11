206382cfb08e5df38b6220022da4ce7d54130baa
adkscherch <adkscherch@gmail.com>
2014-12-11 12:26:34 -0500

    Sound changes to boss hits.

--- Assets/Scripts/Enemy Scripts/EnemyDeath.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDeath.cs
@@ -25,33 +25,40 @@ public class EnemyDeath : MonoBehaviour {
 		points.Notify (new DeathInfo{ shipTag = gameObject.tag, bulletTag = wep.tag, shipPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z)});
 		health -= wep.damage;
 		if (health <= 0) {
-			if(!gameObject.GetComponent<Shooter>().isBoss) { gameObject.layer = LayerMask.NameToLayer("Enemy Bullet"); }
-			if(animation != null) animation.Stop();
-			rigidbody.isKinematic = false;
-			GetComponent<Shooter> ().enabled = false;
-			GetComponent<Wave>().enabled = false;
+				if (!gameObject.GetComponent<Shooter> ().isBoss) {
+						gameObject.layer = LayerMask.NameToLayer ("Enemy Bullet");
+				}
+				if (animation != null)
+						animation.Stop ();
+				rigidbody.isKinematic = false;
+				GetComponent<Shooter> ().enabled = false;
+				GetComponent<Wave> ().enabled = false;
 
-			if(gameObject.GetComponent<Shooter>().isBoss && bossCheck == false) 
-			{ 
-				bossCheck = true;
-				Invoke ("explodeBoss", .5f);
-				Invoke ("explodeBoss", 1f);
-				Invoke ("explodeBoss", 1.5f);
-				Invoke ("explodeBoss", 2f);
-				Invoke ("explodeBoss", 2.5f);
-				Invoke ("explodeBoss", 3f);
-			}
-			if (explosion != null && !gameObject.GetComponent<Shooter>().isBoss) {
-				Instantiate(explosion, transform.position, transform.rotation);
-				explosion = null; // make sure it occurs on the first hit
-			}
-			movement.enabled = false;
+				if (gameObject.GetComponent<Shooter> ().isBoss && bossCheck == false) { 
+						bossCheck = true;
+						Invoke ("explodeBoss", .5f);
+						Invoke ("explodeBoss", 1f);
+						Invoke ("explodeBoss", 1.5f);
+						Invoke ("explodeBoss", 2f);
+						Invoke ("explodeBoss", 2.5f);
+						Invoke ("explodeBoss", 3f);
+				}
+				if (explosion != null && !gameObject.GetComponent<Shooter> ().isBoss) {
+						Instantiate (explosion, transform.position, transform.rotation);
+						explosion = null; // make sure it occurs on the first hit
+				}
+				movement.enabled = false;
 
-			rigidbody.AddForce(new Vector3(Random.Range(-500,500), Random.Range(-350,-200), Random.Range (-250, -100)));
-			Destroy (gameObject, 3f);
-			audio.PlayOneShot(boomSound);
+				rigidbody.AddForce (new Vector3 (Random.Range (-500, 500), Random.Range (-350, -200), Random.Range (-250, -100)));
+				Destroy (gameObject, 3f);
+				audio.PlayOneShot (boomSound);
+		} 
+		else 
+		{
+			if (gameObject.GetComponent<Shooter> ().isBoss) {
+
+			}
 		}
-
 
 	}
 
@@ -60,5 +67,6 @@ public class EnemyDeath : MonoBehaviour {
 		GameObject bossExp = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
 		bossExp.particleEmitter.minSize = 2f;
 		bossExp.particleEmitter.maxSize = 15f;
+		audio.PlayOneShot (boomSound);
 	}
 }
