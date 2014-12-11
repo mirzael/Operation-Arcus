bb3aa62ced6d18a7c9fb974f746e1c5fd9856ce3
unknown <jrh327@71-252-11.drexel.edu>
2014-12-11 00:05:06 -0500

    Centering hiscore textbox

--- Assets/Scripts/Background Scripts/HiscoreTextbox.cs
+++ Assets/Scripts/Background Scripts/HiscoreTextbox.cs
@@ -12,8 +12,8 @@ public class HiscoreTextbox : MonoBehaviour {
 	
 	public void OnGUI () {
 		if (!submitted) {
-			GUI.Label(new Rect(350, 50, 250, 25), "Hiscore! Type your name then press Enter");
-			username = GUI.TextField(new Rect(350, 70, 250, 25), username, 40);
+			GUI.Label(new Rect(100, 50, 250, 25), "Hiscore! Type your name then press Enter");
+			username = GUI.TextField(new Rect(100, 70, 250, 25), username, 40);
 			
 			if (Event.current.keyCode == KeyCode.Return && username.Length > 0) {
 				Debug.Log("Saving score for " + username);
