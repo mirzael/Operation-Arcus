46ba7d1e71438f9af5c3c05b5a722daaf7a67a9a
hubatish <Zach@Aesop.(none)>
2015-02-19 23:20:03 -0500

    updated main menu credits and earth

---
+++ UnityProject/Assets/Programming/Background Scripts/CopyTransform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+    public Transform referenceTransform;
+    protected void Update()
+    {
+        transform.localScale = referenceTransform.localScale;
+    }
+}
+
