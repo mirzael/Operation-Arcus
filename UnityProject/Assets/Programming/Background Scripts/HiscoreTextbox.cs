2f866eb60c3503855cdf257f6f67a0789e5dcbab
hubatish <Zach@Aesop.(none)>
2015-02-22 22:33:44 -0500

    Sort of got win screen working

--- UnityProject/Assets/Programming/Background Scripts/HiscoreTextbox.cs
+++ UnityProject/Assets/Programming/Background Scripts/HiscoreTextbox.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class HiscoreTextbox : MonoBehaviour {
 	private string username = "";
 	public bool submitted = false;
+    public bool inGUI = true;
+    public GameObject thanksSubmitting;
 	
 	public void Awake() {
 		if (!Hiscores.LatestScoreIsHiscore()) {
@@ -10,7 +14,7 @@ public class HiscoreTextbox : MonoBehaviour {
 		}
 	}
 	
-	public void OnGUI () {
+	/*public void OnGUI () {
 		if (!submitted) {
 			GUI.Label(new Rect(100, 50, 250, 25), "Hiscore! Type your name then press Enter");
 			username = GUI.TextField(new Rect(100, 70, 250, 25), username, 40);
@@ -22,5 +26,28 @@ public class HiscoreTextbox : MonoBehaviour {
 				submitted = true;
 			}
 		}
-	}
+	}*/
+
+    public void OnValueChange(string name)
+    {
+        //Debug.LogError("name changed" + name);
+        foreach(var arcus in GameObject.FindObjectsOfType<CharacterDriver>())
+        {
+            arcus.enabled = false;
+        }
+    }
+
+    public void SubmitHiScore(string name)
+    {
+        if(!submitted)
+        {
+            Debug.Log(name + " submitting high score");
+            Hiscores.SaveScore(name, Hiscores.latestScore);
+            if(thanksSubmitting!=null)
+            {
+                thanksSubmitting.SetActive(true);
+                gameObject.SetActive(false);
+            }
+        }
+    }
 }
