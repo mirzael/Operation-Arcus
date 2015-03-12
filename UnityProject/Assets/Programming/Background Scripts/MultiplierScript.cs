3ed39f73df63b904b7763ea6ce1a2ce10be03f9d
mirzael <michael.y.irizarry@gmail.com>
2015-03-12 18:00:44 -0400

    Enemy health is now doubled if you are in multiplayer

--- UnityProject/Assets/Programming/Background Scripts/MultiplierScript.cs
+++ UnityProject/Assets/Programming/Background Scripts/MultiplierScript.cs
@@ -5,4 +5,10 @@ public class MultiplierScript : MonoBehaviour {
 	public float enemyHealthMultiplier;
 	public float enemyCooldownMultiplier;
 	public float playerDamageMultipler;
+
+	public void Start(){
+		if (MultiplayerController.Instance.isMultiplayer) {
+			enemyHealthMultiplier *= 2;
+		}
+	}
 }
