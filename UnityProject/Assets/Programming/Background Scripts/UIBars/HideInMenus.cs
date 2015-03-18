075893c269373211e9ce309a47686223c005a8a3
hubatish <Zach@Aesop.(none)>
2015-03-18 15:51:17 -0400

    Hide in menus for UI bars

---
+++ UnityProject/Assets/Programming/Background Scripts/UIBars/HideInMenus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class HideInMenus : MonoBehaviour
+{
+    protected void Start()
+    {
+        BackgroundUI.Instance.AddGameEndEvent(HideSelf);
+    }
+
+    public void HideSelf()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected void OnDestroy()
+    {
+        if(gameObject.activeSelf)
+        {
+            //if event has already triggered, don't worry about it
+            BackgroundUI.Instance.RemoveGameEndEvent(HideSelf);
+        }
+        GameObject.Destroy(gameObject);
+    }
+}
+
