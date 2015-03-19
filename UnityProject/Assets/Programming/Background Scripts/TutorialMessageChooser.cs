7cca5f04e1de66bafa5bea3889a3439597e6c665
hubatish <Zach@Aesop.(none)>
2015-03-18 23:32:40 -0400

    Forgot to actually add the tutorial message chooser for resh clearly

---
+++ UnityProject/Assets/Programming/Background Scripts/TutorialMessageChooser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TutorialMessageChooser : MonoBehaviour
+{
+    public GameObject onePlayerTwoHandedControlMessage;
+    public GameObject onePlayerOneHandedControlMessage;
+    public GameObject multiplayerTwoHandedControlMessage;
+    public GameObject multiplayerOneHandedControlMessage;
+
+    protected void Start()
+    {
+        onePlayerOneHandedControlMessage.SetActive(false);
+        onePlayerTwoHandedControlMessage.SetActive(false);
+        multiplayerOneHandedControlMessage.SetActive(false);
+        multiplayerOneHandedControlMessage.SetActive(false);
+
+        bool isOneHanded = ControlScheme.isOneHanded;
+        bool isMultiplayer = MultiplayerController.GetMultiplayer();
+
+        if(onePlayerOneHandedControlMessage!=null && isOneHanded && !isMultiplayer)
+        {
+            onePlayerOneHandedControlMessage.SetActive(true);
+        }
+        else if(onePlayerTwoHandedControlMessage!=null && !isOneHanded && !isMultiplayer)
+        {
+            onePlayerTwoHandedControlMessage.SetActive(true);
+        }
+        else if(multiplayerOneHandedControlMessage!=null && isOneHanded && isMultiplayer)
+        {
+            multiplayerOneHandedControlMessage.SetActive(true);
+        }
+        else if(multiplayerTwoHandedControlMessage!=null && !isOneHanded && isMultiplayer)
+        {
+            multiplayerTwoHandedControlMessage.SetActive(true);
+        }
+    }
+}
+
